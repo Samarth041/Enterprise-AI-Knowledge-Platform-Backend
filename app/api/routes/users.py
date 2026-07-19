ec7b28c0c4169a240f5d07090a63d9fc04ac7f66
Samarth041 <samarthgupta826@gmail.com>
2026-07-19 22:49:15 +0530

    added configuration amangement and response model with schema definition

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,37 +1,40 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status
+from app.schemas.user import UserCreate,UserResponse
 
 router=APIRouter(prefix="/users",tags=["Users"])
 
-
-users = [
-    "Alice",
-    "Bob",
-    "Charlie",
-    "David",
-    "Emma",
-    "Frank",
-    "Grace",
-    "Harry",
-    "Ivy",
-    "Jack",
-    "Kevin",
-    "Liam",
-    "Mia",
-    "Noah",
-    "Olivia"
-]
-
 #routing
 @router.get("/")
-def get_users(limit:int=10,skip:int=0):
+def get_users():
+    return[
+        {
+            "id":1,
+            "name":"Alice",
+            "email":"alice@example.com"
+        },
+        {
+            "id":2,
+            "name":"Bob",
+            "email":"bob@example.com"
+        }
+    ]
+
+@router.get("/{user_id}")
+def get_user(user_id:int):
     return{
-        "limit":limit,
-        "skip":skip
+        "id":user_id,
+        "name":"Bob",
+        "email":"bob@example.com"
     }
     
 
 
-#routing parameters
-@router.get("/{user_id}")
-def get_user(user_id:int):
-    return{"user_id":user_id}
+
+
+@router.post("/",response_model=UserResponse,status_code=status.HTTP_201_CREATED)
+def create_user(user:UserCreate):
+    return {
+        "id":1,
+        "name":user.name,
+        "email":user.email
+    }
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.routes.users import router as user_router
+from app.core.config import settings
 
-app=FastAPI(title="Enterprise AI knowledge Platform")
+
+app=FastAPI(title=settings.app_name)
 
 app.include_router(user_router)
