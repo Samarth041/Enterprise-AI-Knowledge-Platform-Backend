e88cab0a2c00e512df170b772abc19e08c3ef489
Samarth041 <samarthgupta826@gmail.com>
2026-07-20 22:37:30 +0530

    also added and tested query parameters

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,14 +1,24 @@
 from fastapi import APIRouter,status,HTTPException
 from app.schemas.user import UserCreate,UserResponse
+from typing import Optional
 
 router=APIRouter(prefix="/users",tags=["Users"])
 fake_db=[]
 
 
 #routing
-@router.get("/")
-def get_users():
-    return fake_db
+@router.get("/",response_model=list[UserResponse])
+def get_users(min_age:Optional[int]=None):
+    if min_age is None:
+        return fake_db
+
+    filtered_users=[]
+
+    for user in fake_db:
+        if user["age"]>=min_age:
+            filtered_users.append(user)
+
+    return filtered_users
 
 @router.get("/{user_id}",response_model=UserResponse)
 def get_user(user_id:int):
@@ -50,3 +60,17 @@ def create_user(user:UserCreate):
 #         "message":"User created successfully",
 #         "user":user
 #     }
+
+#delete user
+
+@router.delete("/{id}",status_code=status.HTTP_200_OK)
+def delete_user(id:int):
+    for index,user in enumerate(fake_db):
+        if user["id"]==id:
+            fake_db.pop(index)
+            return {"message":"User deleted successfully"}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found"
+    )
